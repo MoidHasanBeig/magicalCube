193a24f4bb52a0853378eb9b23780a9f449253d6
MoidHasanBeig <52567265+MoidHasanBeig@users.noreply.github.com>
2019-08-21 21:06:57 +0530

    fix

---
+++ script.js
@@ -0,0 +1,4 @@
+let colArr=["16BAC5","5FBFF9","EFE9F4","171D1C","5863F8","FFB86F","E0CA3C","BA5C12","3E2F5B","261132","17255A","D88373","BD1E1E","3C3C3C","087E8B","C1839F"];
+let boxes= document.querySelectorAll("td");
+let box=document.getElementById("cellFormat");
+document.getElementById("startButton").addEventListener("click", () => box.style.background='red');
